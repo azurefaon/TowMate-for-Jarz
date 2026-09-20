f5eb961bf61378f1f6ba00754ed97965844a3187
donald <cavalidaajune22.com>
2026-09-20 20:32:01 +0800

    Fix APK download content type

--- app/Http/Controllers/AndroidDownloadController.php
+++ app/Http/Controllers/AndroidDownloadController.php
@@ -8,18 +8,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AndroidDownloadController extends Controller
 {
+    private const APK_MIME_TYPE = 'application/vnd.android.package-archive';
+
     public function show(): Response
     {
         $filename = AndroidReleaseService::currentFilename();
 
         if ($filename) {
-            return Storage::disk(AndroidReleaseService::DISK)->download($filename, 'TowMate.apk');
+            return Storage::disk(AndroidReleaseService::DISK)->download($filename, 'TowMate.apk', [
+                'Content-Type' => self::APK_MIME_TYPE,
+            ]);
         }
 
         $legacyPath = AndroidReleaseService::legacyPath();
 
         if (file_exists($legacyPath)) {
-            return response()->download($legacyPath, 'TowMate.apk');
+            return response()->download($legacyPath, 'TowMate.apk', [
+                'Content-Type' => self::APK_MIME_TYPE,
+            ]);
         }
 
         abort(404);
